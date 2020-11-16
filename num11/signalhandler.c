a50cc5bfd5f22de5c3d7ce0bf8281bf42791e352
yejin_ <57592501+yejin1020@users.noreply.github.com>
2020-11-17 05:02:06 +0900

    Update signalhandler.c

--- num11/signalhandler.c
+++ num11/signalhandler.c
@@ -5,30 +5,19 @@
 // 자식 프로세스의 SIGINT, SIGQUIT의 차단을 해제 하는 함수
 //////////////////////////////////////////////////////
 
-void handler_int(int signo){
-    printf("\nSIGINT ON\n");
-    pid_t ppid = getppid();
-    kill(ppid, SIGINT);
-}
-
-void handler_quit(int signo){
-    printf("\nSIGQUIT ON\n");
-    pid_t ppid = getppid();
-    kill(ppid, SIGQUIT);
-}
 
 void signal_int(){
-    struct sigaction sig_int;
-    sig_int.sa_handler = handler_int; 
-    sigfillset(&(sig_int.sa_mask)); 
-    sigaction(SIGINT, &sig_int, NULL);
+	sigset_t set1;
+	sigemptyset(&set1);
+	sigaddset(&set1, SIGINT);//set up set2 with just SIGINT 
+	sigprocmask(SIG_UNBLOCK, &set1, NULL);// unblock SIGINT
 }
 
 void signal_quit(){
-    struct sigaction sig_quit;
-    sig_quit.sa_handler = handler_quit;
-    sigfillset(&(sig_quit.sa_mask));
-    sigaction(SIGQUIT, &sig_quit, NULL);
+	sigset_t set1;
+	sigemptyset(&set1);
+	sigaddset(&set1, SIGQUIT);//set up set2 with just SIGQUIT 
+	sigprocmask(SIG_UNBLOCK, &set1, NULL);// unblock SIGQUIT
 }
 
 /////////////////////////////////////////////////
